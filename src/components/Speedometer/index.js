19977acb2bb1580238c104940578b2beba4f5bf4
gouthami14 <92106741+gouthami14@users.noreply.github.com>
2023-06-02 09:03:29 +0530

    Update index.js

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -9,7 +9,7 @@ class Speedometer extends Component {
     const {count} = this.state
 
     if (count < 200) {
-      this.setState(prevState => ({count: prevState.count - 10}))
+      this.setState(prevState => ({count: prevState.count + 10}))
     }
   }
 
@@ -17,7 +17,7 @@ class Speedometer extends Component {
     const {count} = this.state
 
     if (count < 200) {
-      this.setState(prevState => ({count: prevState.count + 10}))
+      this.setState(prevState => ({count: prevState.count - 10}))
     }
   }
 
@@ -25,26 +25,24 @@ class Speedometer extends Component {
     const {count} = this.state
     return (
       <div className="container">
-        <h1 className="heading">SPEEDDOMETER</h1>
+        <p className="heading">SPEEDOMETER</p>
         <img
           className="image"
+          alt="speedometer"
           src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
         />
-        <h1 className="heading-1">
-          Speed is <span className="counter">{count}</span>mph
-        </h1>
+        <h1 className="heading-1">Speed is {count}mph</h1>
         <p className="description">Min Limit is 0mph, Max Limit is 200mph</p>
         <div className="btn-container">
-          <button className="btn-1" onClick="accelerate">
+          <button className="btn-1" type="button" onClick={this.accelerate}>
             Accelerate
           </button>
-          <button className="btn-2" onClick="applyBreak">
-            Apply Break
+          <button className="btn-2" type="button" onClick={this.applyBreak}>
+            Apply Brake
           </button>
         </div>
       </div>
     )
   }
 }
-
 export default Speedometer
